f010af038470565d27b28888d2688caed19cd783
Sсherbakova Elizaveta Nikolaevna <Sherbakova031193@gmail.com>
2020-02-21 01:40:30 +0300

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Елизавета Щербакова on 21.02.2020.
+//  Copyright © 2020 Sсherbakova Elizaveta Nikolaevna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
